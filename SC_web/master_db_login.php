4d5ad6d88c46ab297625787732d769099f89ced8
dnorcini <dnorcini@kicp.uchicago.edu>
2020-08-10 14:31:17 -0500

    cleaned up mysql user pw

--- SC_web/master_db_login.php
+++ SC_web/master_db_login.php
@@ -3,7 +3,7 @@ date_default_timezone_set('US/Central');    // set this to your server's time zo
 $db_host='localhost';      // fill me in!
 $db_database='control';
 $db_username='control_user';
-$db_password='MyLife4AiurUser';             // fill me in as well!
+$db_password='';             // fill me in as well!
 
 ///  Open up the database connection 
 $connection = mysql_connect($db_host, $db_username, $db_password);
