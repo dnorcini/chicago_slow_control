e550c03d05f431d5f6ca95626e71cabd253799be
dnorcini <dnorcini@kicp.uchicago.edu>
2020-09-15 15:16:53 -0500

    updates from vor

--- SC_backend/slow_control_code/install.sh
+++ SC_backend/slow_control_code/install.sh
@@ -13,11 +13,14 @@ _lib_path="/usr/local/lib"
 #  Edit this if you want the install script somewhere else in your path
 _start_script="/usr/local/bin/slow_start.sh"
 
-ln -s $PWD/lib/lib* $_lib_path
+#ln -s $PWD/lib/lib* $_lib_path
+
+ln -s /home/damic/SC_backend/slow_control_code/lib/lib* /usr/local/lib/
 
 printf "#!/bin/bash\n" > $_start_script
 printf "export LD_LIBRARY_PATH="$_lib_path"\n" >> $_start_script
-printf "$PWD/SC_backend/slow_control_code/SC_Watchdog/SC_Watchdog Watchdog\n" >> $_start_script
+#printf "$PWD/SC_backend/slow_control_code/SC_Watchdog/SC_Watchdog Watchdog\n" >> $_start_script
+printf "/home/damic/SC_backend/slow_control_code/SC_Watchdog/SC_Watchdog Watchdog\n" >> $_start_script
 printf "echo \"Watchdog is now running...\"\n" >> $_start_script
 
 chmod u+x $_start_script
