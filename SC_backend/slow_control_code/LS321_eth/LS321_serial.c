b72e1ecb6eab68290574f268a5512e235ce86f8e
dnorcini <dnorcini@kicp.uchicago.edu>
2020-12-04 09:45:05 -0600

    updated LS321_serial with working version from Michangelo + LS325

--- SC_backend/slow_control_code/LS321_eth/LS321_serial.c
+++ SC_backend/slow_control_code/LS321_eth/LS321_serial.c
@@ -39,7 +39,7 @@ int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
     }
     
     /* set up the serial line parameters :  */
-    tbuf.c_cflag = CS7|B300|CREAD|PARODD;
+    tbuf.c_cflag = CS8|B9600|CREAD|PARODD;
     tbuf.c_iflag = INPCK|ISTRIP;
     tbuf.c_oflag = 0;
     tbuf.c_lflag = 0;
@@ -51,8 +51,27 @@ int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
         exit(1);
     }
 
-    sprintf(cmd_string, "*CLS\r\n");
+    sprintf(cmd_string, "*CLS");
     wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+    wstatus = write(inst_dev, &CR, sizeof(char));
+    wstatus = write(inst_dev, &LF, sizeof(char));    
+    msleep(1000);  
+    //sprintf(cmd_string, "*IDN?");
+    //wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+    // wstatus = write(inst_dev, &CR, sizeof(char));
+    //wstatus = write(inst_dev, &LF, sizeof(char));
+    //msleep(1000);
+    //char       val[33];                      
+    //rdstatus = read(inst_dev, &val, 32); 
+    //fprintf(stdout, "Return from dev: %s \n", val);
+    //msleep(1000);  
+    //char       val2[33];                      
+    //sprintf(cmd_string, "CRDG? B");
+    //wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+    //wstatus = write(inst_dev, &CR, sizeof(char));// \r 
+    //wstatus = write(inst_dev, &LF, sizeof(char));// \n
+    //rdstatus = read(inst_dev, &val2, 32); 
+    //fprintf(stdout, "second Return from dev: %s \n", val2);
     return(0);
 }
 
@@ -63,34 +82,222 @@ void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 }
 
 #define _def_read_sensor
-int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
+int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s_a, double *val_out)
 {
   // Reads out the current value of the Lakeshore temp monitor
     
     char       cmd_string[16];
+    char       ret_string[16];
     char       val[33];                      
     ssize_t    wstatus = 0;
     ssize_t    rdstatus = 0;
 
-    if (strncmp(s_s->subtype, "Temp", 4) == 0)  // Temperatures
-    {	
-	sprintf(cmd_string, "*CDAT?\r\n");
-	fprintf(stdout, "Write to inst: %s \n", cmd_string);
-	wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+    if (strncmp(s_s_a->subtype, "Temp", 4) == 0)  // Temperatures
+    {
+        //printf("yodel");
+      
+        /*
+        sprintf(cmd_string, "*CLS");
+        wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+        wstatus = write(inst_dev, &CR, sizeof(char));
+        wstatus = write(inst_dev, &LF, sizeof(char));
+        msleep(1000);
+        sprintf(cmd_string, "*IDN?");
+        wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+        wstatus = write(inst_dev, &CR, sizeof(char));
+        wstatus = write(inst_dev, &LF, sizeof(char));
+        msleep(1000);
+	
+        rdstatus = read(inst_dev, &val, 32);
+        fprintf(stdout, "Return from dev: %s \n", val);
+        */
+        //msleep(1000);   
+        //      sprintf(cmd_string, "CRDG?%i",s_s_a->num);
+        //      sprintf(cmd_string, "CRDG?");
+        //      sprintf(cmd_string, "KRDG?B");
+        if (s_s_a->num == 1){
+       	    sprintf(cmd_string, "KRDG?A");
+        }
+        else if (s_s_a->num == 2){
+	    sprintf(cmd_string, "KRDG?B");
+	}
+	else{
+	    fprintf(stderr, "%d wrong value for num. Must be 0, 1 or 2 \n", s_s_a->num);
+            return(1);
+	}
+        //      sprintf(cmd_string, "CRDG? B\r\n");
+	//fprintf(stdout, "Write to inst: %s \n", cmd_string);
+        wstatus = write(inst_dev, cmd_string, strlen(cmd_string));
+        wstatus = write(inst_dev, &CR, sizeof(char));
+        wstatus = write(inst_dev, &LF, sizeof(char));
+        
 	msleep(300);  
 	rdstatus = read(inst_dev, &val, 32); 
-	fprintf(stdout, "Return from dev: %s \n", val);
+	/* for (int i =0 ; i< 17; ++i){ */
+	/*   fprintf(stdout, "char = %c \n", val[i]); */
+	/* } */
+	/* fprintf(stdout, "hex = %a \n", val); */
+	/* msleep(300);   */
+	fprintf(stdout, "aaa = %s \n", val);
 	sscanf(val, "%lf", val_out);
+	msleep(1000);  
     }
-    
+    else if (strncmp(s_s_a->subtype, "Heater", 6) == 0) // Read out heater power (in current percentage here)
+    {
+      if (s_s_a->num < 1 || s_s_a->num > 2) // Checks correct Loop number
+	{
+	  fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s_a->num);
+	  return(1);
+	}
+      sprintf(cmd_string, "HTR? %d\n", s_s_a->num);
+      query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      
+      if(sscanf(ret_string, "%lf", val_out) != 1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" in read temperature!\n", ret_string);
+	  return(1);
+	}	
+    }
     else
     {
-	fprintf(stderr, "Wrong subtype for %s \n", s_s->name);
+	fprintf(stderr, "Wrong subtype for %s \n", s_s_a->name);
 	return(1);
     }
     return(0);    
 }
 
+
+#define _def_set_sensor
+int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s_a)
+{
+    char       cmd_string[16];
+    char       ret_string[16];
+    double     ret_val;
+
+    if (strncmp(s_s_a->subtype, "Setpoint", 8) == 0)  // Set the control loop setpoint
+    {
+        if (s_s_a->new_set_val < 0 ) // check valid value for Temp (>0)
+        {
+	    fprintf(stderr, "%f is an incorrect temperature. Temp must be greater than 0... You Fool! \n", s_s_a->new_set_val);
+	    return(1);
+        }
+
+        if (s_s_a->num < 1 || s_s_a->num > 2) // Checks correct Loop number
+        {
+	    fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s_a->num);
+	    return(1);
+        }
+    
+        sprintf(cmd_string, "SETP %d,%f\n", s_s_a->num, s_s_a->new_set_val);
+    
+        write(inst_dev, cmd_string, strlen(cmd_string));
+        sleep(1);
+	
+        sprintf(cmd_string, "SETP? %d\n", s_s_a->num); //queries control loop setpoint
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+        msleep(200);
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+        if(sscanf(ret_string, "%lf", &ret_val) != 1)
+        {
+            fprintf(stderr, "Bad return string: \"%s\" in read setpoint!\n", ret_string);
+            return(1);
+        }
+	
+      /*      if (s_s->new_set_val != 0)
+	if (fabs(ret_val - s_s->new_set_val)/s_s->new_set_val > 0.1)
+	{
+	    fprintf(stderr, "New setpoint of: %f is not equal to read out value of %f\n", s_s->new_set_val, ret_val);
+	    return(1);
+	}
+      */
+    }
+
+    else if (strncmp(s_s_a->subtype, "setramprate", 11) == 0)  // Set the control loop ramprate
+    {
+        if (s_s_a->new_set_val < 0 ) // check valid value for Ramp (>0)
+	{
+       	    fprintf(stderr, "%f is an incorrect ramprate. Ramprate must be greater than 0... You Fool! \n", s_s_a->new_set_val);
+	    return(1);
+	}
+
+        if (s_s_a->num < 1 || s_s_a->num > 2) // Checks correct Loop number
+	{
+	    fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s_a->num);
+	    return(1);
+	}
+    
+        sprintf(cmd_string, "RAMP %d,1,%f\n", s_s_a->num, s_s_a->new_set_val);
+    
+        write(inst_dev, cmd_string, strlen(cmd_string));
+        sleep(1);
+	
+        sprintf(cmd_string, "RAMP? %d\n", s_s_a->num); //queries control loop ramprate
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+        msleep(200);
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+        memmove(&ret_string[0], &ret_string[0]+2, sizeof(ret_string)-2); // output in n,+nnn (on/off,value)
+	    
+        if(sscanf(ret_string, "%lf", &ret_val) != 1)
+	{
+	    fprintf(stderr, "Bad return string: \"%s\" in read ramprate!\n", ret_string);
+	    return(1);
+	}
+	
+        if (s_s_a->new_set_val != 0)
+	  if (fabs(ret_val - s_s_a->new_set_val)/s_s_a->new_set_val > 0.1)
+	  {
+	      fprintf(stderr, "New ramprate of: %f is not equal to read out value of %f\n", s_s_a->new_set_val, ret_val);
+	      return(1);
+	  }
+    }   
+
+    else if (strncmp(s_s_a->subtype, "heateronoff", 11) == 0)  // Set the heater on/off
+    {
+        if (s_s_a->num < 1 || s_s_a->num > 2) // Checks correct Loop number
+        {
+            fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s_a->num);
+            return(1);
+        }
+      
+        sprintf(cmd_string, "RANGE %d,%i\n", s_s_a->num, (int)s_s_a->new_set_val);
+        fprintf(stdout, "%s\n", cmd_string);
+	    
+        write(inst_dev, cmd_string, strlen(cmd_string));
+        sleep(1);
+
+        sprintf(cmd_string, "RANGE? %i\n", s_s_a->num); //queries control loop on/off
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+        msleep(200);
+        query_serial(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+        fprintf(stdout, "Heater status: %s\n", ret_string);
+      
+        if(sscanf(ret_string, "%lf", &ret_val) != 1)
+        {
+            fprintf(stderr, "Bad return string: \"%s\" in read heater onoff!\n", ret_string);
+            return(1);
+        }
+
+        if (s_s_a->new_set_val != 0)
+          if (fabs(ret_val - s_s_a->new_set_val)/s_s_a->new_set_val > 0.1)
+          {
+              fprintf(stderr, "New setpoint of: %f is not equal to read out value of %f\n", s_s_a->new_set_val, ret_val);
+              return(1);
+          }
+    }
+    
+    else       // Print an error if invalid subtype is entered
+    {
+        fprintf(stderr, "Wrong type for %s \n", s_s_a->name);
+        return(1);
+    }
+
+    return(0);
+}
 
 #include "main.h"
 
