5780c0d280866981948982fec48c1c9a69e579e6
dnorcini <dnorcini@kicp.uchicago.edu>
2021-02-15 09:08:54 -0600

    update Agilent for LTA

--- SC_backend/slow_control_code/AgilentE3646/AgilentE3646.c
+++ SC_backend/slow_control_code/AgilentE3646/AgilentE3646.c
@@ -30,16 +30,15 @@ int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 
   sprintf(cmd_string, "*CLS\n");
   write_tcp(inst_dev, cmd_string, strlen(cmd_string));
-  msleep(200);
-
+  msleep(600);
   
   sprintf(cmd_string, "SYST:INT RS232\n");
   write_tcp(inst_dev, cmd_string, strlen(cmd_string));
-  msleep(200);
+  msleep(600);
 
-  //sprintf(cmd_string, "SYST:REM\n");
-  //write_tcp(inst_dev, cmd_string, strlen(cmd_string));
-  //msleep(200);
+  sprintf(cmd_string, "SYST:REM\n");
+  write_tcp(inst_dev, cmd_string, strlen(cmd_string));
+  msleep(600);
 
   return(0);
 }
@@ -56,14 +55,27 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
   char       cmd_string[64];
   char       ret_string[64];                      
 
-  if (strncmp(s_s->subtype, "volt", 4) == 0)  // Read out value for voltage
+  if (strncmp(s_s->subtype, "current", 7) == 0)  // Read out value for current
+   {
+      sprintf(cmd_string, "MEAS:CURR?\n");
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(600);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if(sscanf(ret_string, "%lf", val_out) != 1)
+        {
+          fprintf(stderr, "Bad return string: \"%s\" in read current!\n", ret_string);
+          return(1);
+        }
+    }
+
+  
+  else if (strncmp(s_s->subtype, "radomir", 7) == 0)  // Read out value for voltage
    {
       sprintf(cmd_string, "MEAS:VOLT?\n");
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-      msleep(200);
+      msleep(600);
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-
-      // fprintf(stdout, "Voltage reading: %s\n", ret_string);
 	      
       if(sscanf(ret_string, "%lf", val_out) != 1)
 	{
@@ -71,9 +83,10 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  return(1);
 	}	
     }
-   
+
   else       // Print an error if invalid subtype is entered
     {
+      fprintf(stderr, "Subtype: %s\n", s_s->subtype);
       fprintf(stderr, "Wrong type for %s \n", s_s->name);
       return(1);
     } 
@@ -92,15 +105,14 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
   if (strncmp(s_s->subtype, "setvolt", 7) == 0)  // Set the control loop set voltage
     {    
       sprintf(cmd_string, "VOLT %f\n", s_s->new_set_val);
-    
       write_tcp(inst_dev, cmd_string, strlen(cmd_string));
       sleep(1);
-	
+
       sprintf(cmd_string, "VOLT?\n"); //queries control loop set voltage
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-      msleep(200);
+      msleep(600);
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-
+      
       if(sscanf(ret_string, "%lf", &ret_val) != 1)
 	{
 	  fprintf(stderr, "Bad return string: \"%s\" in read set voltage!\n", ret_string);
@@ -126,17 +138,6 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
       sprintf(cmd_string, "OUTP %i\n", (int)s_s->new_set_val); //need to convert to int, otherwise goes crazy
       write_tcp(inst_dev, cmd_string, strlen(cmd_string));
       sleep(1);
-
-      sprintf(cmd_string, "OUTP?\n"); //queries control loop set on/off
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-      msleep(200);
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-
-      if(sscanf(ret_string, "%lf", &ret_val) != 1)
-        {
-          fprintf(stderr, "Bad return string: \"%s\" in read set on/off!\n", ret_string);
-          return(1);
-        }
     }
 
   else       // Print an error if invalid subtype is entered
