cffa0ca409ea7d36647ae703945951584c5b84cc
Danielle Norcini <dnorcini@kicp.uchicago.edu>
2024-05-02 10:16:44 -0500

    Update WIENER_VME.c

--- SC_backend/slow_control_code/WIENER_VME/WIENER_VME.c
+++ SC_backend/slow_control_code/WIENER_VME/WIENER_VME.c
@@ -14,7 +14,7 @@
 int inst_dev;
 
 #define _def_set_up_inst
-int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)    
+int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 {
   char       cmd_string[64];
 
@@ -32,7 +32,7 @@ void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 {
   close(inst_dev);
 }
- 
+
 #define _def_read_sensor
 int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
 {
@@ -41,16 +41,17 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
   char       buffer[128];
   FILE*      tmp;
 
-    if (strncmp(s_s->subtype, "outvolt",7) == 0)  
+    if (strncmp(s_s->subtype, "outvolt",7) == 0)
     {
       if (s_s->num < 0 || s_s->num > 6 || s_s->num == 2 || s_s->num == 4) // Checks correct sensor number
 	{
 	  fprintf(stderr, "%d is an incorrect value for num. Must between 0, 1, 3, 5. \n", s_s->num);
 	  return(1);
 	}
-      
+
       sprintf(cmd_string,"snmpwalk -v 2c -m +WIENER-CRATE-MIB -c public %s outputMeasurementSenseVoltage.u%i", i_s->dev_address, s_s->num);
-      
+      //sleep(300);
+
       // send snmp command to command line and store response in tmp file
       tmp = popen(cmd_string, "r");
       if (tmp == NULL)
@@ -58,20 +59,20 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  printf("Failed to run command\n" );
 	  exit(1);
 	}
-      
+
       // read response one line at a time
       while (fgets(buffer, sizeof(buffer), tmp) != NULL)
 	{
 	  sprintf(ret_string,buffer);
 	}
-      
+
       // close tmp file
       pclose(tmp);
 
-       // output in WIENER-CRATE-MIB::outputVoltage.u0 = Opaque: Float: 5.000000 V
+      // output in WIENER-CRATE-MIB::outputVoltage.u0 = Opaque: Float: 5.000000 V
       int data_length = strlen(ret_string); //C convert char to int
       memmove(&ret_string[0]+data_length-2, &ret_string[-1], sizeof(ret_string)-data_length); //remove null and units
-      memmove (&ret_string[0], &ret_string[0]+data_length-11, data_length+6); //remove response string
+      memmove (&ret_string[0], &ret_string[0]+data_length-12, data_length+6); //remove response string
 
       if(sscanf(ret_string, "%lf", val_out) != 1)
 	{
@@ -80,16 +81,16 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	}
     }
 
-    else if (strncmp(s_s->subtype, "outcurr",7) == 0)  
+    else if (strncmp(s_s->subtype, "outcurr",7) == 0)
     {
       if (s_s->num < 0 || s_s->num > 6 || s_s->num == 2 || s_s->num == 4) // Checks correct sensor number
 	{
 	  fprintf(stderr, "%d is an incorrect value for num. Must between 0, 1, 3, 5. \n", s_s->num);
 	  return(1);
 	}
-      
+
       sprintf(cmd_string,"snmpwalk -v 2c -m +WIENER-CRATE-MIB -c public %s outputMeasurementCurrent.u%i", i_s->dev_address, s_s->num);
-      
+
       // send snmp command to command line and store response in tmp file
       tmp = popen(cmd_string, "r");
       if (tmp == NULL)
@@ -97,13 +98,13 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  printf("Failed to run command\n" );
 	  exit(1);
 	}
-      
+
       // read response one line at a time
       while (fgets(buffer, sizeof(buffer), tmp) != NULL)
 	{
 	  sprintf(ret_string,buffer);
 	}
-      
+
       // close tmp file
       pclose(tmp);
 
@@ -119,10 +120,10 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	}
     }
 
-    else if (strncmp(s_s->subtype, "fantemp",7) == 0)  
-    {      
+    else if (strncmp(s_s->subtype, "fantemp",7) == 0)
+    {
       sprintf(cmd_string,"snmpwalk -v 2c -m +WIENER-CRATE-MIB -c public %s fanAirTemperature.0", i_s->dev_address);
-      
+
       // send snmp command to command line and store response in tmp file
       tmp = popen(cmd_string, "r");
       if (tmp == NULL)
@@ -130,13 +131,13 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  printf("Failed to run command\n" );
 	  exit(1);
 	}
-      
+
       // read response one line at a time
       while (fgets(buffer, sizeof(buffer), tmp) != NULL)
 	{
 	  sprintf(ret_string,buffer);
 	}
-      
+
       // close tmp file
       pclose(tmp);
 
@@ -151,13 +152,46 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  return(1);
 	}
     }
-    
+
+    else if (strncmp(s_s->subtype, "status",6) == 0)
+    {
+      sprintf(cmd_string,"snmpget -v 2c -m +WIENER-CRATE-MIB -c public %s sysMainSwitch.0", i_s->dev_address);
+
+      // send snmp command to command line and store response in tmp file
+      tmp = popen(cmd_string, "r");
+      if (tmp == NULL)
+        {
+          printf("Failed to run command\n" );
+          exit(1);
+        }
+
+      // read response one line at a time
+      while (fgets(buffer, sizeof(buffer), tmp) != NULL)
+        {
+          sprintf(ret_string,buffer);
+        }
+
+      // close tmp file
+      pclose(tmp);
+
+       // output in WIENER-CRATE-MIB::sysMainSwitch.0 = INTEGER: on(1)
+      int data_length = strlen(ret_string); //C convert char to int
+      memmove(&ret_string[0]+data_length-2, &ret_string[-1], sizeof(ret_string)-data_length); //remove null and units
+      memmove (&ret_string[0], &ret_string[0]+data_length-3, data_length+2); //remove response string
+
+      if(sscanf(ret_string, "%lf", val_out) != 1)
+        {
+          fprintf(stderr, "Bad return string: \"%s\" in read status main switch!\n", ret_string);
+          return(1);
+        }
+    }
+
     else
       {
 	fprintf(stderr, "Wrong type for %s\n", s_s->name);
 	return(1);
       }
-   
+
   return(0);
 }
 
@@ -187,13 +221,13 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 	  printf("Failed to run command\n" );
 	  exit(1);
 	}
-      
+
       	// read response one line at a time
       	while (fgets(buffer, sizeof(buffer), tmp) != NULL)
 	{
 	  sprintf(ret_string,buffer);
 	}
-      
+
       	// close tmp file
       	pclose(tmp);
 
@@ -201,14 +235,14 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 	int data_length = strlen(ret_string); //C convert char to int
       	memmove(&ret_string[0]+data_length-1, &ret_string[-1], sizeof(ret_string)-data_length); //remove null and units
       	memmove (&ret_string[0], &ret_string[0]+data_length-2, data_length+1); //remove response string
-    
+
 
 	if(sscanf(ret_string, "%lf", &ret_val) != 1)
 	{
 	  fprintf(stderr, "Bad return string: \"%s\" in read set main!\n", ret_string);
 	  return(1);
 	}
-	
+
 	if (s_s->new_set_val != 0)
 	  if (fabs(ret_val - s_s->new_set_val)/s_s->new_set_val > 0.1)
 	   {
@@ -216,16 +250,12 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 	     return(1);
 	   }
     }
-   
+
 	 else       // Print an error if invalid subtype is entered
     {
       fprintf(stderr, "Wrong type for %s \n", s_s->name);
       return(1);
-    } 
+    }
 
   return(0);
-}    
-
-
-
-#include "main.h"
+}
