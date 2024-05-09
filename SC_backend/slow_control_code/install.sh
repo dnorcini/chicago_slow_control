5439ace0b41ef135d3935eba6da7a08661310003
damicm <damicm@hopkins-ccd-sc1.jhu.edu>
2024-05-09 11:17:10 -0500

    updated install.sh

--- SC_backend/slow_control_code/install.sh
+++ SC_backend/slow_control_code/install.sh
@@ -19,6 +19,9 @@ ln -s /home/damicm/SC_backend/slow_control_code/lib/lib* /usr/local/lib/
 
 printf "#!/bin/bash\n" > $_start_script
 printf "export LD_LIBRARY_PATH="$_lib_path"\n" >> $_start_script
+printf "#if ungraceful shutdown, reset Watchdog parameters in database so it can restart\n" >> $_start_script
+printf "mysql -u root -p<password> control -Bse \"UPDATE sc_insts SET PID=-1, start_time=-1,last_update_time=-1 WHERE name='Watchdog'\";\n" >> $_start_script
+printf "echo \"Watchdog entry in database reset...\"\n" >> $_start_script
 #printf "$PWD/SC_backend/slow_control_code/SC_Watchdog/SC_Watchdog Watchdog\n" >> $_start_script
 printf "/home/damicm/SC_backend/slow_control_code/SC_Watchdog/SC_Watchdog Watchdog\n" >> $_start_script
 printf "echo \"Watchdog is now running...\"\n" >> $_start_script
